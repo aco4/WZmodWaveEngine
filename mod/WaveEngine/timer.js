31ecc35df31c39ca1d44fcd91dd8e45083609f76
aco4 <anthonythegu@gmail.com>
2026-04-14 17:41:15 -0400

    Decouple time from baseType

--- mod/WaveEngine/timer.js
+++ mod/WaveEngine/timer.js
@@ -2,18 +2,7 @@ namespace("timer_");
 
 function timer_eventStartLevel()
 {
-	if (baseType === CAMP_CLEAN)
-	{
-		setMissionTime(5 * 60);
-	}
-	else if (baseType === CAMP_BASE)
-	{
-		setMissionTime(4 * 60);
-	}
-	else // CAMP_WALLS
-	{
-		setMissionTime(3 * 60);
-	}
+	setMissionTime(5 * 60);
 }
 
 function timer_eventMissionTimeout()
